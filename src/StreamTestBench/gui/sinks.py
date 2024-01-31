7641e811a3d349a64c8d7dcf7b7861c057482cfc
coburnw <rcw@peak.org>
2024-01-30 18:16:00 -0800

    add windowing

--- src/StreamTestBench/gui/sinks.py
+++ src/StreamTestBench/gui/sinks.py
@@ -7,21 +7,26 @@
 # MIT License
 
 import numpy as np
+from matplotlib.widgets import RadioButtons
 
 import StreamTestBench.gui.bench as bench
+
 
 class SpectrumPanel(bench.Panel):
     def __init__(self):
         super().__init__()
         
         self.osr = 32
+        self._window = None  # np.ones(self.stream.samples.shape)
 
+        self.fig = None
+        self.ax = None
         return
     
     def place_on(self, figure):
         self.fig = figure
         
-        self.ax = self.fig.add_subplot() # FFT of bitstream
+        self.ax = self.fig.add_subplot()  # FFT of bitstream
         self.ax.set_xscale('log')
         # ax.set_ylim(-140, 0)
 
@@ -29,10 +34,11 @@ class SpectrumPanel(bench.Panel):
 
     def update(self, stream):
         self.ax.clear()
-        (ms_spec, ms_freqs, trace ) = self.ax.magnitude_spectrum(
+
+        (ms_spec, ms_freqs, trace) = self.ax.magnitude_spectrum(
             stream.samples,
             Fs=1.0/stream.delta_t,
-            window = np.ones(stream.samples.shape),
+            window = self._window,
             scale='dB',
             linewidth=0.4)
 
@@ -53,7 +59,7 @@ class SpectrumPanel(bench.Panel):
         # input is single-sided
         # assume coherent input signal so power in 1 bin
         N = len(mg_spec)
-        #print(N)
+        # print(N)
 
         # Bandwidth
         bwi = int(np.ceil(N/osr))
@@ -84,6 +90,55 @@ class SpectrumAnalyzer(SpectrumPanel):
         self.stream = stream
         self.stream.add_listener(self)
         
+        self.shapes = ('None', 'Bartlett', 'Blackman', 'Hamming', 'Hanning', 'Kaiser')
+        self.shape = self.shapes[0]
+        return
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @shape.setter
+    def shape(self, shape):
+        if shape not in self.shapes:
+            err_str = "'{}'. Unrecognized window shape. Expected one of {}.".format(shape, self.shapes)
+            raise ValueError(err_str)
+
+        index = self.shapes.index(shape)
+        if index == 0:
+            self._window = np.ones(self.stream.samples.shape)
+        elif index == 1:
+            self._window = np.bartlett(self.stream.sample_count)
+        elif index == 2:
+            self._window = np.blackman(self.stream.sample_count)
+        elif index == 3:
+            self._window = np.hamming(self.stream.sample_count)
+        elif index == 4:
+            self._window = np.hanning(self.stream.sample_count)
+        elif index == 5:
+            self._window = np.kaiser(self.stream.sample_count, 14.0)
+
+        self._shape = shape
+        return
+
+    def place_on(self, figure):
+        figs = figure.subfigures(nrows=1, ncols=2, width_ratios=[4, 1])
+
+        super().place_on(figs[0])
+
+        axcolor = 'lightgoldenrodyellow'
+        fig = figs[1]
+        ax = fig.add_axes([0.0, 0.1, 0.9, 0.8], facecolor=axcolor)
+        ax.set_facecolor(axcolor)
+        shape_index = self.shapes.index(self.shape)
+        self.radio = RadioButtons(ax, self.shapes, active=shape_index)
+        self.radio.on_clicked(self.window_change)
+
+        return
+
+    def window_change(self, new_shape):
+        self.shape = new_shape
+        self.stream.update_listeners()
         return
 
             
@@ -119,7 +174,8 @@ class XYDisplay(bench.Panel):
         self.fig.canvas.draw_idle()
 
         return
-    
+
+
 class TestPoints(bench.Panel):
     def __init__(self, streams):
         super().__init__()
@@ -160,7 +216,7 @@ class TestPoints(bench.Panel):
         tick_locations = []
         for i in range(numRows):
             names.append(self.streams[i].name)
-            tick_locations.append( i*self.delta_row)
+            tick_locations.append(i*self.delta_row)
 
             t = self.streams[i].time_series
             offset_data = self.streams[i].samples + (i*self.delta_row)
